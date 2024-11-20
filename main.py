bcbbe07e696c546f4c957ac19305b8686d09357e
eric <accountemail@tutanota.com>
2024-11-20 14:19:51 +0100

    added frames to the gui

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ sg.theme_global("DarkTeal9")
 
 def layout():
     liste = []
-    for i in range(1, 17):
-        liste.append([sg.Input(default_text=f"{i}. Player", key=f"{i}.1", disabled_readonly_background_color="grey"), sg.Input(default_text="", key=f"{i}.2", disabled_readonly_background_color="grey", enable_events=True), sg.Combo(values=["alive", "dead"], default_value="alive", key=f"{i}.3", enable_events=True)])
+    for i in range(1, 17, 2):
+        liste.append([sg.Frame(title=f"{i}. Player", key=f"{i}.1", layout=[[sg.Input(default_text="", key=f"{i}.2", disabled_readonly_background_color="grey", enable_events=True), sg.Combo(values=["alive", "dead"], default_value="alive", key=f"{i}.3", enable_events=True)]])])
+        liste[-1].append(sg.Frame(title=f"{i + 1}. Player", key=f"{i + 1}.1", layout=[[sg.Input(default_text="", key=f"{i + 1}.2", disabled_readonly_background_color="grey", enable_events=True), sg.Combo(values=["alive", "dead"], default_value="alive", key=f"{i + 1}.3", enable_events=True)]]))
 
         if i in [4,8,12]:
             liste.append([sg.VSep()])
@@ -14,7 +15,6 @@ def layout():
     liste.append([sg.Button(button_text="Info", key="info")])
 
     return liste
-
 
 layout = layout()
 
@@ -34,10 +34,8 @@ while True:
         number = e.split(".")
 
         if v[e] == "dead":
-            w[f"{number[0]}.1"].update(disabled=True)
             w[f"{number[0]}.2"].update(disabled=True)
         elif v[e] == "alive":
-            w[f"{number[0]}.1"].update(disabled=False)
             w[f"{number[0]}.2"].update(disabled=False)
 
     if e == "info":
