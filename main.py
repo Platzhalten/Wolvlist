71d234410b6f571f7d5565e00c0f2b131583cf24
eric <accountemail@tutanota.com>
2024-11-30 18:26:33 +0100

    changed the dead dropdown to a checkbox

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ voting_roles = ["Narr", "hh", "Anarchist"]
 def layout():
     liste = []
     for i in range(1, 17, 4):
-        liste.append([sg.Frame(title=f"{i}. Player", key=f"{i}.1", layout=[[sg.Input(default_text="", key=f"{i}.2", disabled_readonly_background_color="grey", enable_events=True, size=20)], [sg.Combo(values=["alive", "dead"], default_value="alive", key=f"{i}.3", enable_events=True), sg.Combo(values=teams, key=f"{i}.4", enable_events=True)]])])
-        liste[-1].append(sg.Frame(title=f"{i + 1}. Player", key=f"{i + 1}.1", layout=[[sg.Input(default_text="", key=f"{i + 1}.2", disabled_readonly_background_color="grey", enable_events=True, size=20)], [sg.Combo(values=["alive", "dead"], default_value="alive", key=f"{i + 1}.3", enable_events=True), sg.Combo(values=teams, key=f"{i + 1}.4", enable_events=True)]]))
-        liste[-1].append(sg.Frame(title=f"{i + 2}. Player", key=f"{i + 2}.1", layout=[[sg.Input(default_text="", key=f"{i + 2}.2", disabled_readonly_background_color="grey", enable_events=True, size=20)], [sg.Combo(values=["alive", "dead"], default_value="alive", key=f"{i + 2}.3", enable_events=True), sg.Combo(values=teams, key=f"{i + 2}.4", enable_events=True)]]))
-        liste[-1].append(sg.Frame(title=f"{i + 3}. Player", key=f"{i + 3}.1", layout=[[sg.Input(default_text="", key=f"{i + 3}.2", disabled_readonly_background_color="grey", enable_events=True, size=20)], [sg.Combo(values=["alive", "dead"], default_value="alive", key=f"{i + 3}.3", enable_events=True), sg.Combo(values=teams, key=f"{i + 3}.4", enable_events=True)]]))
+        liste.append([sg.Frame(title=f"{i}. Player", key=f"{i}.1", layout=[[sg.Input(default_text="", key=f"{i}.2", disabled_readonly_background_color="grey", enable_events=True, size=20)], [sg.Checkbox(text="Dead" , key=f"{i}.3", enable_events=True), sg.Combo(values=teams, key=f"{i}.4", enable_events=True)]])])
+        liste[-1].append(sg.Frame(title=f"{i + 1}. Player", key=f"{i + 1}.1", layout=[[sg.Input(default_text="", key=f"{i + 1}.2", disabled_readonly_background_color="grey", enable_events=True, size=20)], [sg.Checkbox(text="Dead" , key=f"{i + 1}.3", enable_events=True), sg.Combo(values=teams, key=f"{i + 1}.4", enable_events=True)]]))
+        liste[-1].append(sg.Frame(title=f"{i + 2}. Player", key=f"{i + 2}.1", layout=[[sg.Input(default_text="", key=f"{i + 2}.2", disabled_readonly_background_color="grey", enable_events=True, size=20)], [sg.Checkbox(text="Dead" , key=f"{i + 2}.3", enable_events=True), sg.Combo(values=teams, key=f"{i + 2}.4", enable_events=True)]]))
+        liste[-1].append(sg.Frame(title=f"{i + 3}. Player", key=f"{i + 3}.1", layout=[[sg.Input(default_text="", key=f"{i + 3}.2", disabled_readonly_background_color="grey", enable_events=True, size=20)], [sg.Checkbox(text="Dead" , key=f"{i + 3}.3", enable_events=True), sg.Combo(values=teams, key=f"{i + 3}.4", enable_events=True)]]))
 
     liste.append([sg.Input(size=116)])
     liste.append([sg.Input(size=116, key="info out")])
@@ -81,12 +81,15 @@ while True:
 
             if wert:
                 aura_dict[wert].append(i)
-            elif not wert and v[f"{i}.3"] == "alive":
+            elif not wert and v[f"{i}.3"] == False:
                 no_info.append(str(i) + " ")
 
         info_list = []
 
-        w["info left"]("".join(no_info) + "übrig")
+        if no_info:
+            w["info left"]("".join(no_info) + "übrig")
+        else:
+            w["info left"]("Alle wurden überprüft")
 
         for i in aura_dict:
             temp_list = []
